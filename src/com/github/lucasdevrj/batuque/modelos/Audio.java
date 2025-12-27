a7a5e73ba3e1f2d389886802dd96070d8c1298e1
Lucas <lucaspereiradelima2020@gmail.com>
2025-12-27 15:41:07 -0300

    Adição dos atributos.

--- src/com/github/lucasdevrj/batuque/modelos/Audio.java
+++ src/com/github/lucasdevrj/batuque/modelos/Audio.java
@@ -1,4 +1,11 @@
 package com.github.lucasdevrj.batuque.modelos;
 
 public class Audio {
+
+    private String titulo;
+    private int duracao;
+    private int totalDeReproducoes;
+    private int curtidas;
+    private int classificacao;
+
 }
