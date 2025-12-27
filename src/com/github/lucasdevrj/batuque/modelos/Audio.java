a160281b573496d7c7f3940a930b34b3fc029a2d
Lucas <lucaspereiradelima2020@gmail.com>
2025-12-27 15:59:01 -0300

    Instanciando uma Musica.

--- src/com/github/lucasdevrj/batuque/modelos/Audio.java
+++ src/com/github/lucasdevrj/batuque/modelos/Audio.java
@@ -8,21 +8,33 @@ public class Audio {
     private int curtidas;
     private int classificacao;
     private boolean estaReproduzindo;
+    private boolean foiCurtido;
 
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
     public void curtir() {
-        System.out.println("Você curtiu \"" + titulo + "\" com sucesso.");
+        if (!foiCurtido) {
+            System.out.println("Você curtiu \"" + titulo + "\" com sucesso.");
+            foiCurtido = true;
+            curtidas++;
+        } else {
+            System.out.println("Você já curtiu o \"" + titulo + "\".");
+        }
     }
 
     public void reproduzir() {
         if (!estaReproduzindo) {
             System.out.println("Reproduzindo \"" + titulo + "\".");
+            totalDeReproducoes++;
             estaReproduzindo = true;
         } else {
-            System.out.println("O " + titulo + " já esta sendo reproduzido.");
+            System.out.println("O \"" + titulo + "\" já esta sendo reproduzido.");
         }
     }
 
--- src/com/github/lucasdevrj/batuque/principal/Principal.java
+++ src/com/github/lucasdevrj/batuque/principal/Principal.java
@@ -1,11 +1,15 @@
 package com.github.lucasdevrj.batuque.principal;
 
-import com.github.lucasdevrj.batuque.modelos.Audio;
+import com.github.lucasdevrj.batuque.modelos.Musica;
 
 public class Principal {
     public static void main(String[] args) {
-        Audio audio = new Audio();
-        audio.setTitulo("I Gotta Feeling");
-        audio.reproduzir();
+        Musica musica = new Musica();
+        musica.setTitulo("I Gotta Feeling");
+        musica.setDuracao(4);
+        musica.reproduzir();
+        musica.reproduzir();
+        musica.curtir();
+        musica.curtir();
     }
 }
